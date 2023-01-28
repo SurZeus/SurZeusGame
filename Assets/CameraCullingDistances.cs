38ca3a912dd5efeb5772a3cfec8982ffd4938fff
DESKTOP-2B3RR7L\winte <krzysztof.surdyn1999@gmail.com>
2023-01-28 06:56:02 +0100

    reworked

--- Assets/CameraCullingDistances.cs
+++ Assets/CameraCullingDistances.cs
@@ -9,6 +9,8 @@ public class CameraCullingDistances : MonoBehaviour
         Camera camera = GetComponent<Camera>();
         float[] distances = new float[32];
         distances[16] = 50;
+        distances[13] = 50;
+
         camera.layerCullDistances = distances;
     }
 }
